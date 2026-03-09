3dccb2e3b6e4491e50ef2986e55484da7b16b765
kaiser-114 <areezilla@gmail.com>
2026-03-09 15:46:12 +0700

    add inputbmi

--- kai.cpp
+++ kai.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 float berat, tinggi; 
 
+void inputbmi()
+ {
+    cout << "\n--- Selamat Datang Mahasiswa Kelas A 2026 ---" << endl;
+    cout << "Masukkan Berat Badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan Tinggi Badan (m): ";
+    cin >> tinggi;
+
+}
+
+ 
